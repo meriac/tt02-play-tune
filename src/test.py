f4f707700ebb94e8cf62869cf88538a9c25f850f
Milosch Meriac <milosch@meriac.com>
2022-11-14 15:22:08 +0400

    Initial check-in

--- src/test.py
+++ src/test.py
@@ -2,22 +2,15 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 @cocotb.test()
-async def test_7seg(dut):
+async def test_tune(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+    clock = Clock(dut.clk, 100, units="us")
+    cocotb.fork(clock.start())
 
     dut._log.info("reset")
     dut.rst.value = 1
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
 
-    dut._log.info("check all segments")
-    for i in range(10):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, 100)
-        assert int(dut.segments.value) == segments[i]
+    await ClockCycles(dut.clk, 250000)
