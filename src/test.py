be57d0e70b9dcb4328653b477ab5a07a6655506e
Milosch Meriac <milosch@meriac.com>
2022-11-17 08:03:47 +0400

    Switch clock frequency from 3000Hz to 3125Hz

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_tune(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 333, units="us")
+    clock = Clock(dut.clk, 320, units="us")
     cocotb.fork(clock.start())
     
     dut._log.info("reset")
@@ -13,4 +13,4 @@ async def test_tune(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
 
-    await ClockCycles(dut.clk, 75000)
+    await ClockCycles(dut.clk, 78125)
